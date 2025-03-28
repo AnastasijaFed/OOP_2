9475c26adb7a85aaec1138957ca2dbbc23457a3a
AnastasijaFed <nastefedorenko@gmail.com>
2025-03-28 14:12:55 +0200

    inicializuoti kintamieji ir get'eriai set'eriai

--- StudentClass.h
+++ StudentClass.h
@@ -6,13 +6,23 @@
 #define STUDENTCLASS_H
 #include <string>
 #include <vector>
-
+using namespace std;
 
 class StudentClass {
     public:
-      std::string name;
-      std::string surname;
-    private:
+      string name;
+      string surname;
+      string getName(){return name;}
+      string getSurname(){return surname;}
+      void setName(string name){this->name = name;}
+      void setSurname(string surname){this->surname = surname;}
+      vector<uint8_t> getGrades(){return grades;}
+      void setGrades(vector<uint8_t> grades){this->grades = grades;}
+      void setFinalGrade(double finalGrade){this->final_grade = finalGrade;}
+      double getFinalGrade(){return final_grade;}
+
+
+   private:
     std::vector<uint8_t> grades;
     uint8_t exam_grade;
     mutable double final_grade;
